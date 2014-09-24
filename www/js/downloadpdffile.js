79701a038aa8679e746cf2bc3de97665acc4d888
Asim K T <asim@alraislabs.com>
2014-09-24 17:14:13 +0530

    seperated js files

---
+++ www/js/downloadpdffile.js
@@ -0,0 +1,25 @@
+
+            function downloadPDFFile() {
+                alert("called downloadPDFFile");
+                var fileDownloadPDF = new FileTransfer();
+                var uri = encodeURI('http://alrais.host22.com/test.html');
+                var folder = '/sdcard/';
+                var fileName = 'test.txt';
+                var pathToFile = folder + fileName;
+                console.log("Hai")
+                fileDownloadPDF.download(
+                                  uri,
+                                  pathToFile,
+                                  function(entry) {
+                                  console.log("download complete: " + entry.fullPath);
+                                  alert("File Downloaded. Click 'Read Editable Downloaded File' to see text");
+                                  window.plugins.pdfViewer.showPdf(filePath);
+                                  },
+                                  function(error) {
+                                  console.log("download error source " + error.source);
+                                  console.log("download error target " + error.target);
+                                  console.log("upload error code" + error.code);
+                                  alert("Download error!!.")
+                                  }
+                                  );
+            }
